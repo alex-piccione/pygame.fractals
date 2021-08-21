22f92ba23f708a85433b238e68b45768d4ee9592
Alex <alessandro.piccione.75@gmail.com>
2021-08-21 16:06:17 +0100

    Add main.py and VS Code configuration

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Fractal 01")
+
+def drawScreen():
+    screen.fill((0,0,0))
+
+running = True
+while running:
+    drawScreen()
+    for event in pygame.event.get():
+        if (event.type == pygame.KEYDOWN and event.key == pygame.QUIT):
+            running = False
+        
+pygame.quit()
+exit(0)
